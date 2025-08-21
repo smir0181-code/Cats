911cb35820cc63809cb5823989a3faac89c380f3
smir0181-code <smir0181@gmail.com>
2025-08-21 15:39:58 +0500

    add menu

--- Cats.py
+++ Cats.py
@@ -20,6 +20,8 @@ def set_image():
     # img=ImageTk.PhotoImage(img)
         label.config(image=img)
         label.image=img
+def exit():
+    window.destroy()
 
 
 window =Tk()
@@ -27,8 +29,16 @@ window.title("cat")
 window.geometry("600x600")
 label = Label()
 label.pack()
-update_button = Button(text="update",command=set_image)
-update_button.pack()
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=exit)
+
+# update_button = Button(text="update",command=set_image)
+# update_button.pack()
 url='https://cataas.com/cat'
 set_image()
 
