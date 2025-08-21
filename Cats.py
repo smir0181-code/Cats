5d9280243f2e1849d0972d9cff01909a02ca7b24
smir0181-code <smir0181@gmail.com>
2025-08-21 12:51:09 +0500

    create function image

--- Cats.py
+++ Cats.py
@@ -2,8 +2,20 @@ from tkinter import *
 import requests 
 from PIL import ImageTk, Image
 from io import BytesIO
+def load_image():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        img_data = BytesIO(response.content)
+        img = Image.open(BytesIO(img_data))
+        img = img.resize((200, 200), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        return img
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return None
 window =Tk()
-window.title("Weather App")
+window.title("cat")
 window.geometry("500x500")
 label = Label()
 label.pack()
