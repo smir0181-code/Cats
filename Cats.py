72b854f8101f65e48c40492f73e136f73051b52a
smir0181-code <smir0181@gmail.com>
2025-08-21 12:43:47 +0500

    create window

--- Cats.py
+++ Cats.py
@@ -2,5 +2,17 @@ from tkinter import *
 import requests 
 from PIL import ImageTk, Image
 from io import BytesIO
+window =Tk()
+window.title("Weather App")
+window.geometry("500x500")
+label = Label()
+label.pack()
+url='https://cataas.com/cat'
+img=load_image(url)
+if img:
+    # img=ImageTk.PhotoImage(img)
+    label.config(image=img)
+    label.image=img
+window.mainloop()
 
 
